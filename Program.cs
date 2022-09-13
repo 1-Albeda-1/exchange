9228cad4722fff0e399c478309bbfb009241f6bd
Кристина <Kristina.Smirnova1310@yandex.ru>
2022-09-13 16:20:32 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal kurs = 60.75m;
+            decimal kom1 = 8.0m;
+            decimal kom2 = 0.37m;
+            var vneseno = false;
+            var vidano = 0.0m;
+
+            while (!vneseno)
+            {
+                Console.Write("Внесите сумму в долларах для обмена: ");
+                vneseno = decimal.TryParse(Console.ReadLine(), out vidano);
+            }
+            var perevod = vidano * kurs;
+            if (vidano > 500)
+            { 
+                Console.WriteLine("Внесенная сумма больше 500 долларов. Коммисия составит 0,37%.");
+                Console.WriteLine("Выдано: " + (perevod - ((perevod * kom2) / 100)) + "рублей.");  
+            }
+            else
+            {
+               Console.WriteLine("Внесенная сумма меньше 500 долларов. Коммисия составит 8 рублей.");
+               Console.WriteLine("Выдано: " + (perevod - kom1) + "рублей.");
+            }
+            
+        }
+    }
+
+}
